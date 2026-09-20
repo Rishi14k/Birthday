2580a34e8e054045026e722e52706be99a1f0f17
Rishi14k <168741053+Rishi14k@users.noreply.github.com>
2026-09-20 20:27:16 +0530

    ssbh

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
        <ShowtimeCountdown
       
       // change this date to the desired countdown target date
-      showPreviewButton={true}
+      showPreviewButton={false}
       
       targetDate={new Date('2005-09-23T00:00:00+05:30')}
      
